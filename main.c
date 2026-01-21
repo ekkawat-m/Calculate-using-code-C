d5ad067da338eb07e0fce538f2fd3a71f779f930
ekkawat-m <ekawa.m.dev@gmail.com>
2026-01-21 16:18:23 +0700

    Bug fix

--- main.c
+++ main.c
@@ -1,69 +1,53 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
 
-int input_number(char *msg) {
+
+int in(char *m){
     int n;
-    do {
-        printf("%s (1-10): ", msg);
-        scanf("%d", &n);
-    } while (n < 1 || n > 10);
+    do{ printf("%s(1-10): ",m); scanf("%d",&n); }
+    while(n<1||n>10);
     return n;
 }
 
-void wait_enter() {
-    printf("Press Enter...");
-    getchar();
-    getchar();
+void calc(char o){
+     system("cls");
+    int a=in("A "),b=in("B ");
+    printf(o=='+'?"%d+%d=%d":
+           o=='-'?"%d-%d=%d":
+           o=='*'?"%dX%d=%d":
+           o=='/'?"%d/%d=%.2f":"%d%%%d=%d",
+           a,b,o=='/'?(float)a/b:o=='%'?a%b:
+           o=='+'?a+b:o=='-'?a-b:a*b);
+        printf("\nEnter...");
+        getchar();
+        getchar();
 }
 
-void calculator(char op) {
-    int a = input_number("number 1");
-    int b = input_number("number 2");
-
-    switch (op) {
-        case '+': printf("%d + %d = %d\n", a, b, a + b); break;
-        case '-': printf("%d - %d = %d\n", a, b, a - b); break;
-        case '*': printf("%d X %d = %d\n", a, b, a * b); break;
-        case '/': printf("%d div %d = %.2f\n", a, b, (float)a / b); break;
-        case '%': printf("%d mod %d = %d\n", a, b, a % b); break;
-    }
-    wait_enter();
-}
-
-void menu() {
-    char c;
-    while (1) {
+int main(){
+    int t;
+    char p[20],c,op[]="+-*%/";
+    for(t = 4;t--;){
+        printf("Password (%d): ",t+1);
+        scanf("%s",p);
+        if(!strcmp(p,"12345")) break;
         system("cls");
-        printf("1.+  2.-  3.X  4.div  5.mod  6.exit\n");
-        printf("Select: ");
-        scanf(" %c", &c);
-
-        if (c == '6') exit(0);
-
-        char ops[] = {'+', '-', '*', '/', '%'};
-        if (c >= '1' && c <= '5')
-            calculator(ops[c - '1']);
+        if(!t) return 0;
     }
-}
+    while(1){
+             int c;
+             system("cls");
+             printf("1.+ 2.- 3.X 4.div 5.mod 6.exit\nSelect: ");
+             scanf("%d",&c);
 
-void username_password() {
-    char u[20], p[20];
-    int tries = 3;
+             if (c == 6) {
+                system("cls");
+                printf("Bye Bye...\n");
+                Sleep(2000);
+                exit(0);
+            }
 
-    while (tries--) {
-        printf("Password (%d left): ", tries +1);
-        scanf("%s", p);
-
-        if (!strcmp(p, "12345")) {
-            menu();
-            return;
-        }
-        system("cls");
+             if(c>=1 && c<=5) calc(op[c-1]);
     }
-}
-
-int main() {
-    username_password();
-    return 0;
 }
